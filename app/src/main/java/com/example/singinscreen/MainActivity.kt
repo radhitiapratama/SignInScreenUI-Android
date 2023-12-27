a6de2f2b4fc8aba4f958c0c411a1f19a9b04a51f
Radhitia Pratama <radhitia43@gmail.com>
2023-12-27 21:39:48 +0700

    finish design

---
+++ app/src/main/java/com/example/singinscreen/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.singinscreen
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
